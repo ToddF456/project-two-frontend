1fb1a5c40db6456ec55d12491e2066aa24839212
the-fetaverse <ioannis.sporidis@gmail.com>
2022-07-27 13:30:29 -0400

    Added booking modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomepageComponent } from './components/home-page/homepage.component';
 import { BookingFormComponent } from './components/booking-form/booking-form.component';
 import { BookingPageComponent } from './components/booking-page/booking-page.component';
+import { BookingModalComponent } from './components/booking-modal/booking-modal.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomepageComponent, BookingFormComponent, BookingPageComponent],
+  declarations: [AppComponent, NavbarComponent, HomepageComponent, BookingFormComponent, BookingPageComponent, BookingModalComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
