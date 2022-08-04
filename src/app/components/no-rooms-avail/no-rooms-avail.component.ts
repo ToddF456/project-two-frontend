64e5243d2093eeba228bc0cc12da4160250b3b4e
the-fetaverse <ioannis.sporidis@gmail.com>
2022-08-04 10:53:04 -0400

    Redirecting user to no vacancy page if no rooms are available

---
+++ src/app/components/no-rooms-avail/no-rooms-avail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-rooms-avail',
+  templateUrl: './no-rooms-avail.component.html',
+  styleUrls: ['./no-rooms-avail.component.css']
+})
+export class NoRoomsAvailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
